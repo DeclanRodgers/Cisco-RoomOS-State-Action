f3ee7307cba67d05840ad41b26f0c07c7b07d94e
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-03-24 14:52:17 +0000

    test cmpty command csv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const macro1 = require('./macros/macro1/getrequest');
 const macro2 = require('./macros/macro2/postrequest');
 const core = require('@actions/core');  //contains log operations
 
-//let tokenData = Fs.readFileSync(__dirname+'/textfiles/secret.txt')
+//let tokenData = Fs.readFileSync(__dirname+'/textfiles/secret.txt')    //offline testing
 let tokenData = core.getInput('token-data');
 let apiEndpoint = core.getInput('api-endpoint');
 let destinationsCSV = __dirname+'/destinations.csv';
@@ -18,7 +18,7 @@ async function main(){
         try{
             let deviceArray = await extractor.ExtractContents(destinationsCSV);        
             let commandArray = await extractor.ExtractContents(commandsCSV);
-            core.info(`CSV data extracted successfully`)
+            core.info(`CSV data extracted successfully`);
             OutputCalls(deviceArray, commandArray);
         } catch (err){
             core.error(`\tError processing CSV files: ${err.message}`);
@@ -35,14 +35,14 @@ async function OutputCalls(deviceArray, commandArray){
             core.info('\n');
         };
     
-    core.info('** POST Calls **')
-    for(i = 0; i < deviceArray.length; i++){
-        for(j = 0; j < commandArray.length; j++){
-            core.info(`POST Request for device: ${deviceArray[i]} with command(s):\n${commandArray[j]}`);
-            await macro2.SendPostCommand(deviceArray[i], commandArray[j], tokenData, apiEndpoint);
-            core.info('\n');
+        core.info('** POST Calls **')
+        for(i = 0; i < deviceArray.length; i++){
+            for(j = 0; j < commandArray.length; j++){
+                core.info(`POST Request for device: ${deviceArray[i]} with command(s):\n${commandArray[j]}`);
+                await macro2.SendPostCommand(deviceArray[i], commandArray[j], tokenData, apiEndpoint);
+                core.info('\n');
+            };
         };
-    };
     } catch(err){
         core.error(`\t${err.message}`);
     }
