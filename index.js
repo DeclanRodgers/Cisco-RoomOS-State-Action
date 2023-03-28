635925e3e1699a0aee59041a84574793a538c2e1
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-03-28 17:13:31 +0100

    testing invalid device removal

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const macro1 = require('./macros/macro1/getrequest');
 const macro2 = require('./macros/macro2/postrequest');
 const core = require('@actions/core');  //contains log operations
 
-//let tokenData = Fs.readFileSync(__dirname+'/textfiles/secret.txt');    //offline testing
-//let apiEndpoint = "https://app.device-view.com/api/devices/{id}";     //offline testing
-let tokenData = core.getInput('token-data');
-let apiEndpoint = core.getInput('api-endpoint');
+let tokenData = Fs.readFileSync(__dirname+'/textfiles/secret.txt');    //offline testing
+let apiEndpoint = "https://app.device-view.com/api/devices/{id}";     //offline testing
+//let tokenData = core.getInput('token-data');
+//let apiEndpoint = core.getInput('api-endpoint');
 let destinationsCSV = __dirname+'/destinations.csv';
 let commandsCSV = __dirname+'/commands.csv';
 
@@ -28,9 +28,7 @@ async function main(){
 }
 
 async function OutputCalls(deviceArray, commandArray){
-    try{
-        console.log(deviceArray);
-        
+    try{        
         core.info('\n** GET Calls **')    
         for(var i = deviceArray.length - 1; i >= 0; i--){
             core.info(`GET Request for device: ${deviceArray[i]}`);
@@ -42,9 +40,7 @@ async function OutputCalls(deviceArray, commandArray){
             core.info('\n');
         };
 
-        console.log(deviceArray);
-
-        if (deviceArray){
+        if (deviceArray.length === undefined){
             core.info('** POST Calls **')
             for(i = 0; i < deviceArray.length; i++){
                 for(j = 0; j < commandArray.length; j++){
