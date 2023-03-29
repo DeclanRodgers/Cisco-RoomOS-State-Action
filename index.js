474d1fa45e3205b48b47a90ca31cc28561fc2402
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-03-29 10:21:38 +0100

    branch to testing, tidying

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const Fs = require('fs');
+const core = require('@actions/core');  //contains log operations
 const extractor = require('./lib/ExtractCSVContents');
 const macro1 = require('./macros/macro1/getrequest');
 const macro2 = require('./macros/macro2/postrequest');
-const core = require('@actions/core');  //contains log operations
 
-//let tokenData = Fs.readFileSync(__dirname+'/textfiles/secret.txt');    //offline testing
-//let apiEndpoint = "https://app.device-view.com/api/devices/{id}";     //offline testing
+
 let tokenData = core.getInput('token-data');
 let apiEndpoint = core.getInput('api-endpoint');
 let destinationsCSV = __dirname+'/destinations.csv';
 let commandsCSV = __dirname+'/commands.csv';
 
 async function main(){
-    // Check that the file(s) exists 
     if(!Fs.existsSync(destinationsCSV) || !Fs.existsSync(commandsCSV)) {
         core.error(`\tDestinations '${destinationsCSV}' or Commands ${commandsCSV} file not found`);
     } else {        
